1df19e7803e8b32e8fb6960cab091ea3f96860ac
lewkhoonpeng <lewkhoonpeng@yahoo.com>
2021-06-07 15:59:51 +0100

    CORS Error Fixed

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -5,6 +5,7 @@ exports.handler = async (event, context, cb) => {
   //   console.log(context);
   //   console.log(cb);
   return {
+    headers: { "Access-Control-Allow-Origin": "*" }, //CORS Error Fixed
     statusCode: 200,
     body: JSON.stringify(items),
   };
